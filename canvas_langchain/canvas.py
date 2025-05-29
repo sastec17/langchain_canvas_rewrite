429644099f8a03f2676e4c9ab101ab828110fe3f
Sarah Stec <sastec@umich.edu>
2025-05-29 15:07:48 -0400

    Rewrite embed url utility functions, load syllabus

--- canvas_langchain/canvas.py
+++ canvas_langchain/canvas.py
@@ -1,5 +1,39 @@
+from canvasapi import Canvas
+
+from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
+from typing import List
+from sections.syllabus import load_syllabus
+from urllib.parse import urljoin
+
 
 class CanvasLoader(BaseLoader):
-    def __init__(self):
+    def __init__(self,
+                 api_url: str,
+                 api_key: str,
+                 course_id: int, 
+                 index_external_urls: bool):
+        self.api_url = api_url
+        self.api_key = api_key
+        # TODO: DOES THIS NEED TO BE A MEMBER VARIABLE? 
+        self.course_id = course_id
+        self.index_external_urls = index_external_urls
+
+        self.canvas = Canvas(api_url, api_key)
+        self.course = self.canvas.get_course(self.course_id,
+                                             include=['syllabus_body'])
+        self.docs = []
         return
+    
+    def load(self) -> List[Document]:
+        try:
+            metadata = {
+                "canvas": self.canvas,
+                "course": self.course,
+                "course_api": urljoin(self.api_url, f'/courses/{self.course.id}')
+            }
+            # load syllabus
+            self.docs.extend(load_syllabus(metadata))
+        except:
+            print("Something went wrong")
+        return self.docs
---
+++ canvas_langchain/sections/syllabus.py
@@ -0,0 +1,25 @@
+from utils.embedded_media import parse_html_for_text_and_urls
+from langchain.docstore.document import Document
+from urllib.parse import urljoin
+
+def load_syllabus(data):
+    syllabus_docs = []
+    if data.course.syllabus_body: 
+        try:
+            (syllabus_text, embed_urls) = parse_html_for_text_and_urls(canvas=data.canvas,
+                                                                    course=data.course,
+                                                                    html=data.course.syllabus_body)
+            syllabus_url = urljoin(data.course_api, '/assignments/syllabus')
+            if (syllabus_text):
+                syllabus_doc = Document(page_content=syllabus_text,
+                                        metadata={"filename": "Course Syllabus",
+                                                  "source": syllabus_url,
+                                                  "kind": "syllabus"})
+
+                syllabus_docs.append(syllabus_doc)
+            
+            #TODO: APPEND EMBED_URLS TO LOAD & ADD YOUTUBE SUPPORT
+
+        except AttributeError:
+            print("An error occurred")
+    return syllabus_docs
