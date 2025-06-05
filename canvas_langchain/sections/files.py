93360cc8d3c3a8dc590e47b3e699d6e5287fcf00
Sarah Stec <sastec@umich.edu>
2025-06-05 15:04:19 -0400

    Resolve .csv file loading errors

--- canvas_langchain/sections/files.py
+++ canvas_langchain/sections/files.py
@@ -1,8 +1,7 @@
-from io import BytesIO
 import tempfile
+from io import BytesIO
 from PyPDF2 import PdfReader
 from PyPDF2 import errors
-from binascii import Error as binasciiError
 from typing import List
 from urllib.parse import urljoin
 from canvas_langchain.base import BaseSectionLoaderVars
@@ -11,29 +10,27 @@ from canvas_langchain.base import BaseSectionLoader
 from canvasapi.exceptions import CanvasException, ResourceDoesNotExist
 
 from langchain.docstore.document import Document
-from canvas_langchain.utils.common import format_data
 
 from langchain_community.document_loaders import (
     Docx2txtLoader,
     UnstructuredExcelLoader,
     UnstructuredMarkdownLoader,
     UnstructuredPowerPointLoader,
+    CSVLoader
 ) 
 
-
-# TODO: Test .md, .excel, .docx file types
 class FileLoader(BaseSectionLoader):
     def __init__(self, BaseSectionVars: BaseSectionLoaderVars, course_api, invalid_files):
         super().__init__(BaseSectionVars)
         self.invalid_files = invalid_files
         self.course_api = course_api
-        # TODO: REVISE THIS SYSTEM
         self.type_match = {
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" : ".csv",
-            "application/vnd.ms-excel": ".csv",
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document": ".docx",
-            "application/vnd.openxmlformats-officedocument.presentationml.presentation": ".md",
-            "application/vnd.openxmlformats-officedocument.presentationml.presentation": ".pptx"
+            "text/md": "md",
+            "text/csv": "csv",
+            "application/vnd.ms-excel": "excel",
+            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" : "excel",
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "docx",
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation": "pptx"
         }
 
 
@@ -42,19 +39,13 @@ class FileLoader(BaseSectionLoader):
         self.logger.info("Loading files...")
         file_documents = []
         try:
-            # TODO: LOOK INTO NESTED TRY EXCEPTS
             files = self.course.get_files()
             for file in files:
-                try: 
-                    file_documents.extend(self.load_file(file))
-                # exception occurs when file is in a hidden module
-                except ResourceDoesNotExist:
-                    self.logger.info(f"File {file.filename} does not exist")
-                    file_content_type = getattr(file, "content-type")
-                    self.invalid_files.append(f"{file.filename} ({file_content_type})")
+                file_documents.extend(self.load_file(file))
 
         except CanvasException as error:
             self.logger.error("Canvas Exception loading files", error)
+        exit()
         return file_documents
     
 
@@ -73,14 +64,18 @@ class FileLoader(BaseSectionLoader):
                     return self._load_pdf_file(file)
                 elif content_type in self.type_match:
                     return self._load_file_general(file, self.type_match[content_type])
-
+            
+            # exception occurs when file is in a hidden module
             except ResourceDoesNotExist as err:
-                self.logger.info('RESOURCE DOES NOT EXIST - SEE ABOVE', err)
+                self.logger.error("File %s does not exist - likely in hidden module %s", file.filename, err)
+                file_content_type = getattr(file, "content-type")
+                self.invalid_files.append(f"{file.filename} ({file_content_type})")
         return []
 
 
     def _load_rtf_or_text_file(self, file) -> List[Document]:
         """Loads and formats text and rtf file data"""
+        self.logger.debug("Loading rtf or txt file: %s", file.filename)
         file_contents = file.get_contents(binary=False)
         text_document = Document(page_content=file_contents,
                                 metadata={"filename": file.filename, 
@@ -92,6 +87,7 @@ class FileLoader(BaseSectionLoader):
 
     def _load_html_file(self, file) -> List[Document]:
         """Loads and formats html file data"""
+        self.logger.debug("Loading html file: %s", file.filename)
         file_contents = file.get_contents(binary=False)
         file_text, embed_urls = self.parse_html(html=file_contents)
         metadata={"content":file_text,
@@ -100,11 +96,12 @@ class FileLoader(BaseSectionLoader):
                         "kind":"file",
                         "id": file.id}
                     }
-        return format_data(metadata=metadata, embed_urls=embed_urls)
+        return self.format_data(metadata=metadata, embed_urls=embed_urls)
 
 
     def _load_pdf_file(self, file):
         """Loads given pdf file by page"""
+        self.logger.debug("Loading pdf file: %s", file.filename)
         file_contents = file.get_contents(binary=True)
         docs = []
         try:
@@ -118,7 +115,9 @@ class FileLoader(BaseSectionLoader):
                                               "page": i+1}
                                     )
                 docs.append(pdf_page)
-
+        #TODO: binascii error handling needed? 
+        except errors.FileNotDecryptedError as err:
+            self.logger.error("Error: pdf %s is encrypted. Error: %s", file.filename, err)
         except Exception as err:
             self.logger.error("Error loading pdf %s. Error: %s", file.filename, err)
         return docs
@@ -126,6 +125,7 @@ class FileLoader(BaseSectionLoader):
 
     def _load_file_general(self, file, file_type):
         """Loads docx, excel, pptx, and md files"""
+        self.logger.debug("Loading %s file: %s", file_type, file.filename)
         file_contents = file.get_contents(binary=True)
         docs=[]
         with tempfile.TemporaryDirectory() as temp_dir:
@@ -136,13 +136,15 @@ class FileLoader(BaseSectionLoader):
                 binary_file.write(file_contents)
 
             match file_type:
-                case '.csv':
-                    loader = UnstructuredExcelLoader(file_path)
-                case '.docx':
+                case 'csv':
+                    loader = CSVLoader(file_path)
+                case 'excel':
+                    loader = UnstructuredExcelLoader(file_path)         
+                case 'docx':
                     loader=Docx2txtLoader(file_path)
-                case '.md':
+                case 'md':
                     loader = UnstructuredMarkdownLoader(file_path)
-                case '.pptx':
+                case 'pptx':
                     loader=UnstructuredPowerPointLoader(file_path)
 
             docs = loader.load()
